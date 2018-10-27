2b9a01e32fe4bd55bf6a8209f99468ed902ca822
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-10-27 05:59:36 +0530

    modifying compilr.js

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -13,9 +13,11 @@ const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
 
+
+
 for(let contract in output){
   fs.outputJsonSync(
-    path.resolve(buildPath, contract + 'json'),
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
     output[contract]
   );
 }
