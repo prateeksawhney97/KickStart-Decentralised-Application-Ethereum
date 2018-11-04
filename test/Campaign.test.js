1a8c34fd6d7ffe696e277b672be16a23c3c469ee
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2018-11-04 12:35:51 +0530

    adding first test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -22,9 +22,16 @@ beforeEach(async ()=> {
       gas: '1000000'
     });
 
-    [campaignAddress] = await factory.methods.getDeployedContracts().call();
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
     campaign = await new web3.eth.Contract(
       JSON.parse(compiledCampaign.interface),
       campaignAddress
     );
+});
+
+describe('Campaigns', ()=> {
+  it('deploys a factory and a campaign', ()=> {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address);
+  });
 });
